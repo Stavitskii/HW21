e45ef189f8dd850da59f545c7c4343c190388425
Stavitskii <pochta100@mail.ru>
2022-08-28 21:40:15 +0300

    00:10:17

---
+++ classes.py
@@ -0,0 +1,49 @@
+from abc import ABC, abstractmethod
+
+
+class Storage(ABC):
+    @abstractmethod
+    def add(self, name, count):
+        pass
+
+    @abstractmethod
+    def remove(self, name, count):
+        pass
+
+    @abstractmethod
+    def get_free_space(self):
+        pass
+
+    @abstractmethod
+    def get_items(self):
+        pass
+
+    @abstractmethod
+    def get_unique_items_count(self):
+        pass
+
+
+class Store(Storage):
+    def __init__(self, items: dict, capacity=100):
+        self.__items = items
+        self.__capacity = capacity
+
+    def add(self, name, count):
+        if self.get_free_space() >= count
+            self.__items[name] += count
+        else:
+            return "Not enough space in the storage"
+
+
+    def get_free_space(self):
+        current_space = 0
+        for value in self.__items.values():
+            current_space += value
+        return self.__capacity - current_space
+
+
+
+
+
+
+
