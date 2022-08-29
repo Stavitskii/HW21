74c21299088b5f21cebe795ceb6a66ead18d7e7e
Stavitskii <pochta100@mail.ru>
2022-08-29 07:07:05 +0300

    00:19:35

--- classes.py
+++ classes.py
@@ -29,11 +29,23 @@ class Store(Storage, ABC):
         self.__capacity = capacity
 
     def add(self, name, count):
-        if self.get_free_space() >= count:
-            self.__items[name] += count
+        if name in self.__items.keys():
+            if self.get_free_space() >= count:
+                self.__items[name] += count
+            else:
+                return "Not enough space in the storage"
         else:
-            return "Not enough space in the storage"
+            if self.get_free_space() >= count:
+                self.__items[name] = count
+            else:
+                return "Not enough space in the storage"
 
+    def remove(self, name, count):
+        if self.__items[name] >= count:
+            self.__items[name] -= count
+
+        else:
+            return "Not enough goods in the storage"
 
     def get_free_space(self):
         current_space = 0
@@ -41,9 +53,40 @@ class Store(Storage, ABC):
             current_space += value
         return self.__capacity - current_space
 
+    def get_items(self):
+        return self.__items
+
+    def get_unique_items_count(self):
+        return len(self.__items.keys())
+
+    def __repr__(self):
+        goods_in_store = ""
+        for key, value in self.__items.items():
+            goods_in_store += f"{key}:{value}, "
+        return goods_in_store
 
 
 
 
+store_1 = Store(items={"cheese": 10, "milk": 70}, capacity=90)
 
+print(store_1.get_free_space())
+print(store_1.get_items())
+print(store_1.get_unique_items_count())
+print(f"store_1 {store_1}\n")
 
+store_1.add("apple", 5)
+print("After add")
+
+print(store_1.get_free_space())
+print(store_1.get_items())
+print(store_1.get_unique_items_count())
+print(f"store_1 {store_1}\n")
+
+store_1.remove("cheese", 10)
+print("After remove")
+
+print(store_1.get_free_space())
+print(store_1.get_items())
+print(store_1.get_unique_items_count())
+print(f"store_1 {store_1}\n")
