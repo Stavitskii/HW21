7ed34d314855e518e12e6f3fdae88d9d1573ee70
Stavitskii <pochta100@mail.ru>
2022-08-29 11:35:19 +0300

    00:23:35

--- classes.py
+++ classes.py
@@ -65,28 +65,63 @@ class Store(Storage, ABC):
             goods_in_store += f"{key}:{value}, "
         return goods_in_store
 
+class Shop(Store):
+    def __init__(self, items, capacity=20):
+        super().__init__(items, capacity)
+
+    def add(self, name, count):
+        if self.get_unique_items_count() > 5:
+            return "Adds impossible, to many different items at shop"
+        else:
+            super().add(self, name, count)
+
+    # def __repr__(self):
+    #     goods_in_shop = ""
+    #     for key, value in self.__items.items():
+    #         goods_in_shop += f"{key}:{value}, "
+    #     return goods_in_shop
 
 
 
-store_1 = Store(items={"cheese": 10, "milk": 70}, capacity=90)
+        # if name in self.__items.keys():
+        #     if self.get_free_space() >= count:
+        #         self.__items[name] += count
+        #     else:
+        #         return "Not enough space in the storage"
+        # else:
+        #     if self.get_free_space() >= count:
+        #         self.__items[name] = count
+        #     else:
+        #         return "Not enough space in the storage"
 
-print(store_1.get_free_space())
-print(store_1.get_items())
-print(store_1.get_unique_items_count())
-print(f"store_1 {store_1}\n")
 
-store_1.add("apple", 5)
-print("After add")
 
-print(store_1.get_free_space())
-print(store_1.get_items())
-print(store_1.get_unique_items_count())
-print(f"store_1 {store_1}\n")
 
-store_1.remove("cheese", 10)
-print("After remove")
+# store_1 = Store(items={"cheese": 10, "milk": 70}, capacity=90)
+#
+# print(store_1.get_free_space())
+# print(store_1.get_items())
+# print(store_1.get_unique_items_count())
+# print(f"store_1 {store_1}\n")
+#
+# store_1.add("apple", 5)
+# print("After add")
+#
+# print(store_1.get_free_space())
+# print(store_1.get_items())
+# print(store_1.get_unique_items_count())
+# print(f"store_1 {store_1}\n")
+#
+# store_1.remove("cheese", 10)
+# print("After remove")
+#
+# print(store_1.get_free_space())
+# print(store_1.get_items())
+# print(store_1.get_unique_items_count())
+# print(f"store_1 {store_1}\n")
 
-print(store_1.get_free_space())
-print(store_1.get_items())
-print(store_1.get_unique_items_count())
-print(f"store_1 {store_1}\n")
+shop_1 = Shop(items={}, capacity=0)
+
+shop_1.add("apple", 5)
+
+print(f"shop_1 {shop_1}\n")
