fa3e68896205a0be62e22117518174e3cc1730f4
Stavitskii <pochta100@mail.ru>
2022-08-30 06:57:10 +0300

    00:46:32

--- classes.py
+++ classes.py
@@ -33,11 +33,13 @@ class Store(Storage, ABC):
             if self.get_free_space() >= count:
                 self.__items[name] += count
             else:
+                print("Not enough space in the storage")
                 return "Not enough space in the storage"
         else:
             if self.get_free_space() >= count:
                 self.__items[name] = count
             else:
+                print("Not enough space in the storage")
                 return "Not enough space in the storage"
 
     def remove(self, name, count):
@@ -45,6 +47,7 @@ class Store(Storage, ABC):
             self.__items[name] -= count
 
         else:
+            print("Not enough goods in the storage")
             return "Not enough goods in the storage"
 
     def get_free_space(self):
@@ -62,66 +65,43 @@ class Store(Storage, ABC):
     def __repr__(self):
         goods_in_store = ""
         for key, value in self.__items.items():
-            goods_in_store += f"{key}:{value}, "
+            goods_in_store += f"{key}:{value}\n"
         return goods_in_store
+
 
 class Shop(Store):
     def __init__(self, items, capacity=20):
         super().__init__(items, capacity)
 
     def add(self, name, count):
-        if self.get_unique_items_count() > 5:
-            return "Adds impossible, to many different items at shop"
+        if self.get_unique_items_count() >= 5:
+            print("Adds impossible, to many different items at the shop")
+            return "Adds impossible, to many different items at the shop"
         else:
-            super().add(self, name, count)
 
-    # def __repr__(self):
-    #     goods_in_shop = ""
-    #     for key, value in self.__items.items():
-    #         goods_in_shop += f"{key}:{value}, "
-    #     return goods_in_shop
+            super().add(name, count)
 
-
-
-        # if name in self.__items.keys():
-        #     if self.get_free_space() >= count:
-        #         self.__items[name] += count
-        #     else:
-        #         return "Not enough space in the storage"
-        # else:
-        #     if self.get_free_space() >= count:
-        #         self.__items[name] = count
-        #     else:
-        #         return "Not enough space in the storage"
+class Request:
+    def __init__(self, request_str):
+        req_list = request_str.split()
+        action = req_list[0]
+        self.__count = int(req_list[1])
+        self.__item = req_list[2]
+        if action == "Supply":
+            self.__from = req_list[4]
+            self.__to = req_list[6]
+        elif action == "Take":
+            self.__from = req_list[4]
+            self.__to = None
+        elif action == "Bring":
+            self.__to = req_list[6]
+            self.__from = None
 
 
 
 
-# store_1 = Store(items={"cheese": 10, "milk": 70}, capacity=90)
-#
-# print(store_1.get_free_space())
-# print(store_1.get_items())
-# print(store_1.get_unique_items_count())
-# print(f"store_1 {store_1}\n")
-#
-# store_1.add("apple", 5)
-# print("After add")
-#
-# print(store_1.get_free_space())
-# print(store_1.get_items())
-# print(store_1.get_unique_items_count())
-# print(f"store_1 {store_1}\n")
-#
-# store_1.remove("cheese", 10)
-# print("After remove")
-#
-# print(store_1.get_free_space())
-# print(store_1.get_items())
-# print(store_1.get_unique_items_count())
-# print(f"store_1 {store_1}\n")
 
-shop_1 = Shop(items={}, capacity=0)
 
-shop_1.add("apple", 5)
 
-print(f"shop_1 {shop_1}\n")
+
+
