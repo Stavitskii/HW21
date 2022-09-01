57c7031975eccff083cf2d3fa98f714d7781ea30
Stavitskii <pochta100@mail.ru>
2022-09-01 16:45:59 +0300

    01:04 almost all works

--- classes.py
+++ classes.py
@@ -32,24 +32,25 @@ class Store(Storage, ABC):
         if name in self.__items.keys():
             if self.get_free_space() >= count:
                 self.__items[name] += count
+                return True
             else:
                 print("Not enough space in the storage")
-                return "Not enough space in the storage"
+                return False
         else:
             if self.get_free_space() >= count:
                 self.__items[name] = count
-
+                return True
             else:
                 print("Not enough space in the storage")
-                return "Not enough space in the storage"
+                return False
 
     def remove(self, name, count):
         if self.__items[name] >= count:
             self.__items[name] -= count
-            print(f"Goods in the store are enough")
+            return True
         else:
             print("Not enough goods in the storage")
-            return "Not enough goods in the storage"
+            return False
 
     def get_free_space(self):
         current_space = 0
@@ -77,10 +78,12 @@ class Shop(Store):
     def add(self, name, count):
         if self.get_unique_items_count() >= 5:
             print("Adds impossible, to many different items at the shop")
-            return "Adds impossible, to many different items at the shop"
+            return False
         else:
-
-            super().add(name, count)
+            if super().add(name, count):
+                return True
+            else:
+                return False
 
 
 class Request:
@@ -94,20 +97,28 @@ class Request:
 
     def move(self):
         try:
-            eval(self.__from).remove(self.__item, self.__count)
-            print(f"A courier took {self.__count} {self.__item} from {self.__from}")
-            print(f"A courier is getting {self.__count} {self.__item} from {self.__from} to {self.__to}")
+            if eval(self.__from).remove(self.__item, self.__count):
+                print(f"Goods in the store are enough")
+                print(f"A courier took {self.__count} {self.__item} from {self.__from}")
+                print(f"A courier is getting {self.__count} {self.__item} from {self.__from} to {self.__to}")
+                if eval(self.__to).add(self.__item, self.__count):
+                    print(f"A courier have taken {self.__count} {self.__item} to {self.__to}")
+                store_items = str(eval(self.__from).get_items)
+                print(store_items)
         except:
             print("Something is wrong with the order")
-        try:
-            eval(self.__to).add(self.__item, self.__count)
-            print(f"A courier have taken {self.__count} {self.__item} to {self.__to}")
-        except:
-            print("Something is wrong with the shop")
-        print(self.__from)
 
 
 
+
+store_1 = Store(items={"cheese": 10, "milk": 10, "apple": 10})
+store_2 = Store(items={"cheese": 10, "milk": 10, "onions": 10})
+shop_1 = Shop(items={"cheese": 3, "milk": 3, "apple": 3}, capacity=7)
+
+
+test_text = "Bring 4 cheese from store_1 to shop_1"
+req = Request(test_text)
+req.move()
 
 
 
